0b01fcf0c2582454783bfa38e3b2b396be3cea03
Paul Phillips <paulp@improving.org>
2013-11-04 10:03:26 -0800

    Fixed bash for mavericks.

---
+++ bash42-std-readline.rb
@@ -0,0 +1,37 @@
+require 'formula'
+
+class Bash42StdReadline < Formula
+  homepage 'http://www.gnu.org/software/bash/'
+  url 'http://ftpmirror.gnu.org/bash/bash-4.2.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/bash/bash-4.2.tar.gz'
+  sha256 'a27a1179ec9c0830c65c6aa5d7dab60f7ce1a2a608618570f96bfa72e95ab3d8'
+  version '4.2.45'
+
+  head 'git://git.savannah.gnu.org/bash.git'
+
+  # Vendor the patches. The mirrors are unreliable for getting the patches,
+  # and the more patches there are, the more unreliable they get. Upstream
+  # patches can be found in: http://ftpmirror.gnu.org/bash/bash-4.2-patches
+  def patches
+    { :p0 => "https://gist.github.com/jacknagel/4008180/raw/1509a257060aa94e5349250306cce9eb884c837d/bash-4.2-001-045.patch" }
+  end unless build.head?
+
+  def install
+    # When built with SSH_SOURCE_BASHRC, bash will source ~/.bashrc when
+    # it's non-interactively from sshd.  This allows the user to set
+    # environment variables prior to running the command (e.g. PATH).  The
+    # /bin/bash that ships with Mac OS X defines this, and without it, some
+    # things (e.g. git+ssh) will break if the user sets their default shell to
+    # Homebrew's bash instead of /bin/bash.
+    ENV.append_to_cflags "-DSSH_SOURCE_BASHRC"
+
+    system "./configure", "--prefix=#{prefix}"
+    system "make install"
+  end
+
+  def caveats; <<-EOS.undent
+    In order to use this build of bash as your login shell,
+    it must be added to /etc/shells.
+    EOS
+  end
+end
