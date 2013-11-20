10078768a258c2cbb51a2ff30b6802a9bed95af8
Paul Phillips <paulp@improving.org>
2013-11-20 13:49:01 -0800

    New formula: stdbuf

---
+++ stdbuf.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Stdbuf < Formula
+  homepage 'http://www.gnu.org/software/coreutils/manual/coreutils.html#stdbuf-invocation'
+  head 'https://github.com/paulp/stdbuf-osx.git'
+  version 'FreeBSD-9.2.0'
+
+  def install
+    system "make"
+    bin.install "stdbuf"
+    lib.install "libstdbuf.dylib"
+  end
+end
