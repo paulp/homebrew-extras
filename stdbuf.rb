7d3c383aa46474e3e16102ecce8973bbf0f576f6
Paul Phillips <paulp@improving.org>
2013-11-23 08:46:39 -0800

    Move stdbuf

--- stdbuf.rb
+++ stdbuf.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Stdbuf < Formula
   homepage 'http://www.gnu.org/software/coreutils/manual/coreutils.html#stdbuf-invocation'
-  head 'https://github.com/paulp/stdbuf-osx.git'
+  head 'https://github.com/tcreech/stdbuf-osx.git'
   version 'FreeBSD-9.2.0'
 
   def install
