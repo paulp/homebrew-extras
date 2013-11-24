93507b4de479342233933413c8717d87910d322b
Paul Phillips <paulp@improving.org>
2013-11-24 14:29:39 -0800

    I guess I need to bump the version for it to be seen.

--- subl.rb
+++ subl.rb
@@ -4,7 +4,7 @@ class Subl < Formula
   homepage 'https://github.com/paulp/'
   url 'https://raw.github.com/paulp/homebrew-extras/master/bin/subl'
   sha1 'f0a78544b940c46f200585ad589679702af7bdd5'
-  version '0.1'
+  version '0.1.1'
 
   def install
     bin.install "subl" => "subl"
