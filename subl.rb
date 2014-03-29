47e9e8d3e444c8113eebe395059d58fc436c6f53
Paul Phillips <paulp@improving.org>
2014-03-29 12:19:05 -0700

    ...and bump version.

--- subl.rb
+++ subl.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Subl < Formula
   homepage 'https://github.com/paulp/'
   url 'https://raw.github.com/paulp/homebrew-extras/master/bin/subl'
-  sha1 'e3e418336810c8bad390446a1b760b7aae6cbeaf'
-  version '0.1.2'
+  sha1 '4864e89a533a50e57ed4c2ddbd37e52d1db39b1c'
+  version '0.1.3'
 
   def install
     bin.install "subl" => "subl"
