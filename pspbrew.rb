3fef0a000165ca09310109a4d2a05def583da602
Paul Phillips <paulp@improving.org>
2014-04-22 13:37:44 -0700

    brew-install-scala.
    And other wrapper-oriented programs for pspbrew.

--- pspbrew.rb
+++ pspbrew.rb
@@ -4,6 +4,6 @@ class Pspbrew < Formula
   head 'git://github.com/paulp/homebrew-extras.git'
 
   def install
-    bin.install "bin/brew-options-built" => "brew-options-built"
+    bin.install Dir['bin/*']
   end
 end
