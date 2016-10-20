39b75bf75a66d16801266b6da302c13f7c6444ff
Paul Phillips <paulp@improving.org>
2016-10-20 14:52:10 -0700

    Maintenance.

--- checker-framework.rb
+++ checker-framework.rb
@@ -1,9 +1,9 @@
 class CheckerFramework < Formula
   desc "Custom pluggable types for Java"
   homepage "http://checkerframework.org/"
-  url "http://types.cs.washington.edu/checker-framework/current/checker-framework.zip"
-  version "1.9.2"
-  sha256 "2fbabcd7192708cb5610ea38c03397f04794aba14f3694ae8dd1d11184b108e9"
+  url "http://types.cs.washington.edu/checker-framework/current/checker-framework-2.1.4.zip"
+  version "2.1.4"
+  sha256 "0801e4b54ba8e8bd54a00a5fccc46febba74877634256b8b4b83cc96571fe376"
   depends_on :java => "1.7+"
 
   def install
