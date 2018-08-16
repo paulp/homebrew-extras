f7df57db823235ea867abdd24fb27dfe47303bc1
Paul Phillips <psp@brb.fyi>
2018-08-15 17:13:01 -0700

    Coursier 1.0.1.

--- coursier.rb
+++ coursier.rb
@@ -3,9 +3,9 @@ require 'formula'
 class Coursier < Formula
   desc "Coursier launcher."
   homepage "http://get-coursier.io"
-  version "1.0.0"
-  url "https://github.com/coursier/coursier/raw/v1.0.0/coursier", :using => :nounzip
-  sha256 "32cf24bbd4221f1d36129e326baeff908eb84ee99dd686ae19a3e339ee63c704"
+  version "1.0.1"
+  url "https://github.com/coursier/coursier/raw/v1.0.1/coursier", :using => :nounzip
+  sha256 "23aa05a84a9df5d84b76be40659300bfcb367d8a1b4307908f89ba3fc6dac1e6"
 
   depends_on :java => "1.8+"
 
@@ -15,7 +15,7 @@ class Coursier < Formula
 
   test do
     ENV["COURSIER_CACHE"] = "#{testpath}/cache"
-    output = shell_output("#{bin}/coursier launch io.get-coursier:echo:1.0.0 -- foo")
+    output = shell_output("#{bin}/coursier launch io.get-coursier:echo:1.0.1 -- foo")
     assert_equal ["foo\n"], output.lines
   end
 end
