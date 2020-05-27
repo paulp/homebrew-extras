3cc87739e3e2fef2f0085b5be1c1c9c919ecf5b0
Paul Phillips <psp@brb.fyi>
2020-05-27 13:45:03 -0700

    z3 update.

--- z3.rb
+++ z3.rb
@@ -1,14 +1,22 @@
 class Z3 < Formula
   desc "High-performance theorem prover (built with java bindings enabled)"
   homepage "https://github.com/Z3Prover/z3"
-  url "https://github.com/Z3Prover/z3/archive/z3-4.8.7.tar.gz"
-  sha256 "8c1c49a1eccf5d8b952dadadba3552b0eac67482b8a29eaad62aa7343a0732c3"
+  url "https://github.com/Z3Prover/z3/archive/z3-4.8.8.tar.gz"
+  sha256 "6962facdcdea287c5eeb1583debe33ee23043144d0e5308344e6a8ee4503bcff"
   head "https://github.com/Z3Prover/z3.git"
 
-  depends_on :java => "1.8"
+  depends_on :java => "1.8+"
 
   def install
-    system "python", "scripts/mk_make.py", "--prefix=#{prefix}", "--staticlib", "--java"
+  	python3 = Formula["python@3.8"].opt_bin/"python3"
+  	xy = Language::Python.major_minor_version python3
+  	system python3, "scripts/mk_make.py",
+  	                 "--prefix=#{prefix}",
+  	                 "--python",
+  	                 "--pypkgdir=#{lib}/python#{xy}/site-packages",
+  	                 "--staticlib",
+  	                 "--java"
+
     cd "build" do
       system "make"
       system "make", "install"
@@ -22,4 +30,5 @@ class Z3 < Formula
            pkgshare/"examples/c/test_capi.c", "-o", testpath/"test"
     system "./test"
   end
+
 end
