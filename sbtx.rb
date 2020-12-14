bbdcb488e8f6e0b93428b5782ac65ef56b4d06fe
Colin Dean <git@cad.cx>
2020-12-13 20:28:34 -0500

    Updates sbtx to late 2020 release

--- sbtx.rb
+++ sbtx.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Sbtx < Formula
   homepage "https://github.com/paulp/sbt-extras"
-  url "https://raw.githubusercontent.com/paulp/sbt-extras/53f1edc685d8f269ac551336809e34faf7cd29a6/sbt"
-  version "20190913"
-  sha256 "243e3fd25262ac7f7f564ff04c363c804a8d96a8821576c3334375a03f451373"
+  url "https://raw.githubusercontent.com/paulp/sbt-extras/e5306b75866a81e54c29b1691bc604acc498b51c/sbt"
+  version "20201123"
+  sha256 "14635fea215f1417c3dd114f80b11d7a81bf88c2ba1650dc5f4a57a7c784ea89"
   head "https://git.io/sbt", :using => :curl
 
   def install
