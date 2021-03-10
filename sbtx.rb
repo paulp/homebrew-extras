7cc5632d7e932dd25b0e243275d9319056a96204
Dale Wijnand <dale.wijnand@gmail.com>
2021-03-10 13:30:44 +0000

    Update sbtx formula

--- sbtx.rb
+++ sbtx.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Sbtx < Formula
   homepage "https://github.com/paulp/sbt-extras"
-  url "https://raw.githubusercontent.com/paulp/sbt-extras/e5306b75866a81e54c29b1691bc604acc498b51c/sbt"
-  version "20201123"
-  sha256 "14635fea215f1417c3dd114f80b11d7a81bf88c2ba1650dc5f4a57a7c784ea89"
+  url "https://raw.githubusercontent.com/paulp/sbt-extras/6db3d3d1c38082dd4c49cce9933738d9bff50065/sbt"
+  version "20210308"
+  sha256 "c41366a625c0abdee2d2ec217c81dbe4e2c297c45717b9f4ee4349db33cdca98"
   head "https://git.io/sbt", :using => :curl
 
   def install
